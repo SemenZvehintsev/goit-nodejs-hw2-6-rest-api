98e8735e797627ffbb9e3ff716339682c3f4a6cb
SemenZvehintsev <kvadratniibomzh@gmail.com>
2023-03-17 11:20:09 +0200

    first commit

---
+++ models/contactModel.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose');
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Set email for contact'],
+    },
+    phone: {
+      type: String,
+      required: [true, 'Set phone for contact'],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false}
+);
+
+const Contact = model('contacts', contactSchema);
+
+module.exports = Contact;
